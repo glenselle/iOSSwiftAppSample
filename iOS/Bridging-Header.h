e4f2d21f433959fff54140bbb17b28d9a12c89e1
Glen Selle <glenselle@me.com>
2016-03-23 12:23:29 -0500

    Added the bridging header

---
+++ iOS/Bridging-Header.h
@@ -0,0 +1,30 @@
+//
+//  Bridging-Header.h
+//  iOS
+//
+
+#import "GTMNSString+HTML.h"
+
+#import "AFNetworking.h"
+
+#import "UIKit+AFNetworking.h"
+
+#import "PNImports.h"
+
+#import "KeenClient.h"
+
+#import "UserVoice.h"
+
+#import "UVStyleSheet.h"
+
+//#import "UICKeyChainStore.h"
+
+#import "SwipeView.h"
+
+#import "IBActionSheet.h"
+
+#import "DateTools.h"
+
+#import "JTSImageViewController.h"
+
+#import "JTSAnimatedGIFUtility.h"
